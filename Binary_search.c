fd99189059775a41c28c363a418fee2af06d7df9
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2023-05-07 11:16:33 +0530

    Create Binary_search.c
    In the binary search algorithm, we first sort the array in non-decreasing order. Then, we search for the given key in the middle of the array. If the middle element is equal to the key, we return the index of the middle element. If the middle element is less than the key, then we search for the key in the right half of the array. If the middle element is greater than the key, then we search for the key in the left half of the array. We repeat this process until the key is found or the search space is exhausted.

---
+++ Binary_search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// Returns the index of the key if found, else -1
+int binarySearch(int arr[], int low, int high, int key) {
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;  // key not found
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 5;
+    int index = binarySearch(arr, 0, n-1, key);
+    if (index == -1) {
+        printf("Key not found");
+    } else {
+        printf("Key found at index %d", index);
+    }
+    return 0;
+}
