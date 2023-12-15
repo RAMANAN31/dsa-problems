b6c8a82555ff4beae9faac5c8ee8bac75727a0d7
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2023-12-15 19:22:32 +0530

    Update Binary_search.c

--- Binary_search.c
+++ Binary_search.c
@@ -6,7 +6,8 @@ int binarySearch(int arr[], int low, int high, int key) {
         int mid = (low + high) / 2;
         if (arr[mid] == key) {
             return mid;
-        } else if (arr[mid] < key) {
+        } 
+        else if (arr[mid] < key) {
             low = mid + 1;
         } else {
             high = mid - 1;
